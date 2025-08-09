525e872c6c56eb3c45eb0f79bcf73a7591420230
Marcos Fernández <marcosfernandezc1998@gmail.com>
2025-08-08 21:02:16 -0300

    movies.json agregado e importado en app.jsx y estructura de componentes creada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import movies from "../movies.json";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
+  console.log(movies);
   return <></>;
 }
 
---
+++ src/components/FiltroEstrellas.jsx
@@ -0,0 +1,8 @@
+export default function () {
+  // logica
+  return (
+    <>
+      {/* aca escribimos todos los componentes que queremos imprimir en pantalla */}
+    </>
+  );
+}
