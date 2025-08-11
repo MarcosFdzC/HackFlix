faff3ca90df5a82899eb1f4f15fc96d880e804bc
Marcos Fernández <marcosfernandezc1998@gmail.com>
2025-08-11 20:05:21 -0300

    componente Pelicula y ListaPeliculas funcionales, falta personalizar diseño y refactorizar con el fetch

--- src/components/ListaPeliculas.jsx
+++ src/components/ListaPeliculas.jsx
@@ -4,14 +4,16 @@ import Pelicula from "./Pelicula";
 export default function () {
   // logica
   return (
-    <>
-      {movies.map((peli) => (
-        <Pelicula
-          key={peli.id}
-          img={peli.poster_path}
-          titulo={peli.title}
-        ></Pelicula>
-      ))}
-    </>
+    <div className="container text-center">
+      <div className="row">
+        {movies.map((peli) => (
+          <Pelicula
+            key={peli.id}
+            img={peli.poster_path}
+            titulo={peli.title}
+          ></Pelicula>
+        ))}
+      </div>
+    </div>
   );
 }
--- src/components/Pelicula.jsx
+++ src/components/Pelicula.jsx
@@ -1,13 +1,13 @@
 //Imports
 import movies from "../../movies.json";
 
-export default function (img, titulo) {
+export default function ({ img, titulo }) {
   // logica
   //console.log(titulo);
   return (
-    <>
+    <div className="col">
       <div className="card" style={{ width: "15rem" }}>
-        <img src={img} className="card-img-top" alt="..." />
+        <img src={img} className="card-img-top" alt={titulo} />
         <div className="card-body">
           <h5 className="card-title">{titulo}</h5>
           <a href="#" className="btn btn-warning">
@@ -15,6 +15,6 @@ export default function (img, titulo) {
           </a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
